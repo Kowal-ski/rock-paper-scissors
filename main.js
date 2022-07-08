9b6ed7a312ca985c5f5fa0d79edb4e488aec9ff0
Kowal-ski <np.40027072@gmail.com>
2022-07-08 13:20:49 +0100

    Edit main.js, Add game logic
    Remove convert function and map rock, paper,
    scissors to numbers for numerical calculation.
    
    Add playRound function, takes input from
    computerPlay and playerPlay functions and
    calculates the winner numerically using modulo
    operator, 0 means ties, 1 means player wins, 2
    means computer wins. 0,1 and 2 are used in switch
    statement for winner declaration.

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
+//Game logic map:
+//rock = 1
+//paper = 2
+//scissors = 3
+
 function computerPlay () {
     let randomNum = Math.random();
     
     if (randomNum <= 0.33) {
-        return('rock');
-    }
-    else if (randomNum > 0.33 && randomNum <= 0.66) {
-        return('paper');
-    }
-    else {
-        return ('scissors')
-    }
-}
-
-function playerPlay () {
-    let playerSelection = (prompt('Please enter: rock, paper or scissors')).toLowerCase();
-
-    if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
-        return playerSelection;
-    }
-    else {
-        while (playerSelection != 'rock' || playerSelection != 'paper' || playerSelection != 'scissors') {
-            playerSelection = (prompt('That was an invalid choice, try again!')).toLowerCase();      
-            if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') break;        
-        }    
-        return playerSelection;
-    }
-}
-
-function convert (input) {
-    if (input == ' rock') {
         return 1;
     }
-    else if (input == 'paper') {
+    else if (randomNum > 0.33 && randomNum <= 0.66) {
         return 2;
     }
     else {
@@ -39,3 +17,38 @@ function convert (input) {
     }
 }
 
+function playerPlay () {
+    let playerSelection = (prompt('Please enter: 1 - rock, 2 - paper or 3 - scissors')).toLowerCase();
+
+    if (playerSelection == 1 || playerSelection == 2 || playerSelection == 3) {
+        return playerSelection;
+    }
+    else {
+        while (playerSelection != 1 || playerSelection != 2 || playerSelection != 3) {
+            playerSelection = (prompt('That was an invalid choice, try again!')).toLowerCase();      
+            if (playerSelection == 1 || playerSelection == 2 || playerSelection == 3) break;        
+        }    
+        return playerSelection;
+    }
+}
+
+function playRound () {
+    let computerSelection = computerPlay();
+    let playerSelection = playerPlay();
+    let calculate = (((playerSelection - computerSelection) % 3) + 3) % 3;
+
+    switch (calculate) {
+        case 0:
+            return ('Its a tie! you got lucky ');
+            break;
+        case 1:
+            return ('You win, you probably cheated...');
+            break;
+        case 2:
+            return ('I win! you suck ');
+            break;
+        default:
+            return ('error...');
+            break;
+    }
+}
